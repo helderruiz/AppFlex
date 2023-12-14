326c4a6f68800eabd569d39ef9238bd20d956e29
HELDER RUIZ MARCELINO <110069838+helderruiz@users.noreply.github.com>
2023-12-14 18:15:54 -0300

    Projeto Finalizado
    Utilizado HTML, CSS, JS

---
+++ script.js
@@ -0,0 +1,23 @@
+let etanol = document.querySelector('#etanol').value
+let gasolina = document.querySelector('#gasolina')
+let imagem = document.querySelector('#imagem')
+const botaoVantegem = document.querySelector('#btnVantagem')
+const botaoLimpar = document.querySelector('#btnLimpar')
+
+
+
+
+botaoVantegem.addEventListener('click', ()=> {
+    etanol = parseFloat(flex.etanol.value)
+    gasolina = parseFloat(flex.gasolina.value)
+
+    if(etanol < 0.7 * gasolina){
+        imagem.setAttribute('src', 'img/etanol.png')
+    }else{
+        imagem.setAttribute('src', 'img/gasolina.png')
+    }
+})
+
+botaoLimpar.addEventListener('click', ()=> {
+    imagem.setAttribute('src', 'img/neutro.png')
+})
